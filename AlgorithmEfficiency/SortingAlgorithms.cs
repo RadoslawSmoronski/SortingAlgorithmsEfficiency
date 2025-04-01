68dc50e7fbbd21189cf6e7b17c398fae64c2b897
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-04-01 10:54:24 +0200

    Create SortingAlgorithms class.

---
+++ AlgorithmEfficiency/SortingAlgorithms.cs
@@ -0,0 +1,114 @@
+﻿
+namespace SortingAlgorithmsEfficiency
+{
+    public class SortingAlgorithms
+    {
+
+        //InsertionSort
+        public int[] InsertionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+        //MergeSort
+        public int[] MergeSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                MergeSort(array, left, middle);
+                MergeSort(array, middle + 1, right);
+                MergeArray(array, left, middle, right);
+            }
+            return array;
+        }
+
+        private int[] MergeArray(int[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
+
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+
+            i = 0;
+            j = 0;
+            int k = left;
+
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+
+            return array;
+        }
+
+        //QuickSort
+        public int[] QuickSort(int[] array, int left, int right)
+        {
+            var i = left;
+            var j = right;
+            var pivot = array[left];
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+                QuickSort(array, left, j);
+            if (i < right)
+                QuickSort(array, i, right);
+            return array;
+        }
+    }
+}
