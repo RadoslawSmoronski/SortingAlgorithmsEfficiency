9548f089424bfad9b06e82b44f482c410d3af5b2
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-04-01 08:38:41 +0200

    Add project files.

---
+++ AlgorithmEfficiency/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmEfficiency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
